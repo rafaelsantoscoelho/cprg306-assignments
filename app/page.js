0d931bc0c8f4dfe87a9e1135486869ddea1bf512
Rafael Santos Coelho <rafael.santoscoelho@edu.sait.ca>
2024-05-17 12:14:09 -0600

    Deleted the original code and inserted a heading and a linkto the week-2 page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <p>Hello World!</p>
+    <main className="h-screen">
+      <h1 className="text-xl">CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="./week-2/">Week 2 Assignment</Link>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+      <main className="h-screen">
+        <p>Rafael Santos Coelho</p>
+        <Link href="https://github.com/rafaelsantoscoelho">My GitHub account</Link>
+      </main>
+    );
+  }
