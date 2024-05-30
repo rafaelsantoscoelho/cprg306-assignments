d4b5a36b8b6a1dcab8dd73977bd26225ad9dfa85
Rafael Santos Coelho <rafael.santoscoelho@edu.sait.ca>
2024-05-30 16:41:31 -0600

    week 3 assignment
    Fixed the white stripe at the bottom of the page

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ export default function Page()
 {
     return (
         <main className="bg-violet-900">
-            <div>
+            <div className="p-5">
                 <h2 className="text-3xl font-bold m-2 text-white">Shopping List</h2>
                 <ItemList />
             </div>
