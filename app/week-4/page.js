4ccb3842dec8886b555124ff0776031a93b1e759
Rafael Santos Coelho <rafael.santoscoelho@edu.sait.ca>
2024-06-07 11:17:10 -0600

    week-4 changed colors

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -4,7 +4,7 @@ import NewItem from "./new-item";
 export default function Page()
 {
     return (
-        <main className="flex justify-center w-full bg-black">
+        <main className="flex justify-center w-full h-screen bg-green-900">
             <NewItem />
         </main>
     );
