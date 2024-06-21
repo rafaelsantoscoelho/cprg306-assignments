b89da7fbb3534997ef1893b930cde503127a7174
Rafael Santos Coelho <rafael.santoscoelho@edu.sait.ca>
2024-06-21 14:45:04 -0600

    week 5 assignment

--- app/week-5/item.js
+++ app/week-5/item.js
@@ -1,7 +1,7 @@
-export default function Item({id, name, quantity, category})
+export default function Item({name, quantity, category})
 {
     return (
-        <li key={id} className="p-2 m-4 bg-slate-900 max-w-sm">
+        <li className="p-2 m-4 bg-slate-900 max-w-sm">
             <h2  className="text-xl text-white font-bold">{name}</h2>
             <div  className="text-sm  text-white italic">Buy {quantity} in {category}</div>
         </li>
