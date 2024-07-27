21cf1a88ce540f513bcef0567e5a005bcbd7f072
Rafael Santos Coelho <rafael.santoscoelho@edu.sait.ca>
2024-07-26 19:31:00 -0600

    Update firebase.js

--- app/week-8/_utils/firebase.js
+++ app/week-8/_utils/firebase.js
@@ -13,6 +13,14 @@ import { getAuth } from "firebase/auth";
 // structure for now, but if the problem persists, please replace
 // the environment variables with the following values for testing
 // purposes
+// I'll just paste the environment variables below (I know that I'm not supposed to do this in real-life production code)
+
+// NEXT_PUBLIC_FIREBASE_API_KEY="AIzaSyDbBCqZRa0uAO_DS-vHMEyRkkg_a-os4QQ"
+// NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN="rscoelho-webdev2-2024.firebaseapp.com"
+// NEXT_PUBLIC_FIREBASE_PROJECT_ID="rscoelho-webdev2-2024"
+// NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET="rscoelho-webdev2-2024.appspot.com"
+// NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID="1049154292715"
+// NEXT_PUBLIC_FIREBASE_APP_ID="1:1049154292715:web:82ccf52b11b6d45c86e7ae"
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
