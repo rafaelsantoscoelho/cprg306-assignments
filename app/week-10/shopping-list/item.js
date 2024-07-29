798aafe26f549a8b1d0d20cb14587fc8c6c44ba6
Rafael Santos Coelho <rafael.santoscoelho@edu.sait.ca>
2024-07-28 20:49:38 -0600

    Week 10

--- app/week-10/shopping-list/item.js
+++ app/week-10/shopping-list/item.js
@@ -1,7 +1,7 @@
 export default function Item({item, onSelect})
 {
     return (
-        <li key={item.id} className="p-2 m-4 bg-slate-900 max-w-sm" onClick={onSelect}>
+        <li key={item.id} className="p-2 m-4 bg-slate-900 max-w-sm hover:bg-orange-500" onClick={onSelect}>
             <h2  className="text-xl text-white font-bold">{item.name}</h2>
             <div  className="text-sm  text-white italic"> Buy {item.quantity} in {item.category}</div>
         </li>
